4cb17eb3dcb2513ebd3aee49ac5187fe037f56a2
adobias7811 <adobias11@gmail.com>
2021-05-19 19:15:06 -0400

    Removed additional containers & styling (temp) & fixed icon images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Calcutale Date & Time
+///////// Calcutale Date & Time
 
 function formatDate(timestamp) {
   let date = new Date(timestamp);
@@ -26,7 +26,31 @@ function formatDate(timestamp) {
   currentDate.innerHTML = `${day} ${hour}:${minutes}`;
 }
 
-// Default View - Display Current Weather Default by Geo-location
+////////// Display Specified Weather for Location
+
+function showWeather(response) {
+  console.log(response.data);
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#current-temp");
+  let weatherCondition = document.querySelector("#weather-description");
+  let maxTemp = Math.round(response.data.main.temp_max);
+  let maxTempElement = document.querySelector("#high-temp");
+  let minTemp = Math.round(response.data.main.temp_min);
+  let minTempElement = document.querySelector("#low-temp");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeed = Math.round(response.data.wind.speed);
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#current-day");
+
+  temperatureElement.innerHTML = `${temperature}°F`;
+  weatherCondition.innerHTML = response.data.weather[0].main;
+  maxTempElement.innerHTML = `${maxTemp}°F`;
+  minTempElement.innerHTML = `${minTemp}°F`;
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windSpeedElement.innerHTML = `${windSpeed} mph`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+////////// Default View - Display Current Weather Default by Geo-location
 
 function retrievePosition(position) {
   let latitude = position.coords.latitude;
@@ -43,30 +67,7 @@ function retrievePosition(position) {
 }
 navigator.geolocation.getCurrentPosition(retrievePosition);
 
-//////////// Search & Display Specified Weather for Location
-
-function showWeather(response) {
-  console.log(response.data);
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#current-temp");
-  let weatherDescription = document.querySelector("#weather-description");
-  let maxTemp = Math.round(response.data.main.temp_max);
-  let maxTempElement = document.querySelector("#high-temp");
-  let minTemp = Math.round(response.data.main.temp_min);
-  let minTempElement = document.querySelector("#low-temp");
-  let humidityElement = document.querySelector("#humidity");
-  let windSpeed = Math.round(response.data.wind.speed);
-  let windSpeedElement = document.querySelector("#wind-speed");
-  let dateElement = document.querySelector("#current-day");
-
-  temperatureElement.innerHTML = `${temperature}°F`;
-  weatherDescription.innerHTML = response.data.weather[0].description;
-  maxTempElement.innerHTML = `${maxTemp}°F`;
-  minTempElement.innerHTML = `${minTemp}°F`;
-  humidityElement.innerHTML = `${response.data.main.humidity}%`;
-  windSpeedElement.innerHTML = `${windSpeed} mph`;
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-}
+//////////// Search for desired city
 
 function searchCity(event) {
   event.preventDefault();
@@ -86,230 +87,3 @@ searchForm.addEventListener("submit", searchCity);
 
 ///////////////
 //Weather Condition Icons
-
-let weatherCondition = response.data.weather[0].main;
-if (weatherCondition === "Clear") {
-  document.querySelector("#weather-description").innerHTML = "Sunny";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/sunny.svg");
-  document.querySelector("#background").classList.add("containerSunny");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerCloudy",
-      "containerPartlyCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Clouds") {
-  document.querySelector("#weather-description").innerHTML = "Cloudy";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/cloudy.svg");
-  document.querySelector("#background").classList.add("containerCloudy");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerPartlyCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Mist") {
-  document.querySelector("#weather-description").innerHTML = "Misty";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/chancerain.svg");
-  document.querySelector("#background").classList.add("containerCloudy");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerPartlyCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Smoke") {
-  document.querySelector("#weather-description").innerHTML = "Smokey";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/cloudy.svg");
-  document.querySelector("#background").classList.add("containerCloudy");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerPartlyCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Haze") {
-  document.querySelector("#weather-description").innerHTML = "Hazy";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/hazy.svg");
-  document.querySelector("#background").classList.add("containerCloudy");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerPartlyCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Dust") {
-  document.querySelector("#weather-description").innerHTML = "Dusty";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/hazy.svg");
-  document.querySelector("#background").classList.add("containerPartlyCloudy");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Fog") {
-  document.querySelector("#weather-description").innerHTML = "Foggy";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/fog.svg");
-  document.querySelector("#background").classList.add("containerCloudy");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerPartlyCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Sand") {
-  document.querySelector("#weather-description").innerHTML = "Sandy";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/hazy.svg");
-  document.querySelector("#background").classList.add("containerCloudy");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerPartlyCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Ash") {
-  document.querySelector("#current-weather-icon").innerHTML = "Ashy";
-  document
-    .querySelector("#main-image")
-    .setAttribute("src", "images/cloudy.svg");
-  document.querySelector("#background").classList.add("containerSnowing");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerCloudy",
-      "containerPartlyCloudy",
-      "containerRaining"
-    );
-}
-if (weatherSituation === "Squall") {
-  document.querySelector("#weather-description").innerHTML = "Windy";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/tstorms.svg");
-  document.querySelector("#background").classList.add("containerRaining");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerCloudy",
-      "containerRaining",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Tornado") {
-  document.querySelector("#weather-description").innerHTML = "Tornado";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/chancestorms.svg");
-  document.querySelector("#background").classList.add("containerRaining");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerCloudy",
-      "containerPartlyCloudy",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Snow") {
-  document.querySelector("#weather-description").innerHTML = "Snowy";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/snow.svg");
-  document.querySelector("#background").classList.add("containerSnowing");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerCloudy",
-      "containerPartlyCloudy",
-      "containerRaining"
-    );
-}
-if (weatherSituation === "Rain") {
-  document.querySelector("#weather-description").innerHTML = "Rainy";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/rain/svg");
-  document.querySelector("#background").classList.add("containerRaining");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerCloudy",
-      "containerPartlyCloudy",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Drizzle") {
-  document.querySelector("#weather-description").innerHTML = "Drizzly";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/chancerain.svg");
-  document.querySelector("#background").classList.add("containerRaining");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerCloudy",
-      "containerPartlyCloudy",
-      "containerSnowing"
-    );
-}
-if (weatherSituation === "Thunderstorm") {
-  document.querySelector("#weather-description").innerHTML = "Stormy";
-  document
-    .querySelector("#current-weather-icon")
-    .setAttribute("src", "images/tstorms.svg");
-  document.querySelector("#background").classList.add("containerRaining");
-  document
-    .querySelector("#background")
-    .classList.remove(
-      "containerSunny",
-      "containerCloudy",
-      "containerPartlyCloudy",
-      "containerSnowing"
-    );
-}
